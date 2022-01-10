05bf601c58d6bde80baf12d11f4137ad002d6d13
Brisa Galarza <brisagalarza@gmail.com>
2022-01-10 17:30:44 -0300

    desafio3

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function NavBar (){
+    return(
+        <>
+            <div className="NavBar">
+                <h2>Kamizama Store</h2>
+                <div className="NavBar-menu">
+                    <a>Inicio</a>
+                    <a>Mi Perfil</a>
+                    <a>Contacto</a>
+                </div>
+            </div>
+        </>
+    )
+}
